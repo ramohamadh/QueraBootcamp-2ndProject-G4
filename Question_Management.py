a7ca2b8b9280d83cedb0528994e8f9496dfe736f
Farzam <farzam.hajisabili@gmail.com>
2024-10-25 00:29:18 +0330

    flask app now use db instead of dictionaries

--- Question_Management.py
+++ Question_Management.py
@@ -1,30 +1,48 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash , g
 from dataclasses import dataclass
+import sqlite3
 
 app = Flask (__name__)
 app.secret_key = 'fwugyewqlufywqliugfqw'
 
-#============================
-last_id = 7
-Questions = [
-    { 'id': '1', 'Q': '2+2', 'A': '4', 'category': 'Math' },
-    { 'id': '2', 'Q': '3+4', 'A': '7', 'category': 'Math' },
-    { 'id': '3', 'Q': 'Capital of France?', 'A': 'Paris', 'category': 'Geography' },
-    { 'id': '4', 'Q': 'Author of Hamlet?', 'A': 'Shakespeare', 'category': 'Literature' },
-    { 'id': '5', 'Q': 'Capital of England?', 'A': 'London', 'category': 'Geography' },
-    { 'id': '6', 'Q': '10x10', 'A': '100', 'category': 'Math' },
-    { 'id': '7', 'Q': '10/5', 'A': '2', 'category': 'Math' }
-]
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect('quiz.db')
+        g.db.row_factory = sqlite3.Row
+        # print("Row factory is set")
+    return g.db
 
-@dataclass
-class User:
-    username: str
-    password: str
-    is_login: bool
-    role: str
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop('db', None)
+    if exception:
+        print(f'error: {exception}')
+    if db is not None:
+        db.close()
 
-current_user = User('admin', 'admin', True, 'admin')
-#============================
+#==================
+@app.route('/currentUser')
+def get_current_user(user: str):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('UPDATE users SET login=? WHERE username=?', (True, user))
+    cursor.execute('SELECT * FROM users WHERE username = ?', (user,))
+    current_user = cursor.fetchone()
+    return current_user
+
+
+def admin_check(user):
+
+    current_user = get_current_user(user)
+
+    if current_user['login'] == 1:
+        if current_user['admin'] == 1:
+            return True
+    return False
+#===========================
+
+
+
 
 
 @app.route('/')
@@ -35,80 +53,91 @@ def index():
 
 @app.route('/questions/<category>')
 def Question_manager(category):
-    if current_user.is_login:
-        if current_user.role == 'admin':
-            filtered_questions = [q for q in Questions if q['category'] == category]
-            return render_template('show_questions.html', Questions=filtered_questions, category=category)
+    if admin_check('admin'):
+        cursor = get_db().cursor()
+        cursor.execute('SELECT * FROM questions WHERE category=?', (category,))
+        filtered_questions = cursor.fetchall()
+        return render_template('show_questions.html', Questions=filtered_questions, category=category)
     else:
         return render_template ('not_allowed.html')
     
 
 @app.route('/categories')
 def show_categories():
-    if current_user.is_login:
-        if current_user.role == 'admin':
-            categories = {q['category'] for q in Questions}
-            return render_template('show_categories.html', categories=categories)
+    if admin_check('admin'):
+        cursor = get_db().cursor()
+        cursor.execute('SELECT * FROM categories')
+        categories = cursor.fetchall()
+
+        return render_template('show_categories.html', categories=categories)
     else:
         return render_template ('not_allowed.html')
 
 
 @app.route ('/questions/add/<category>', methods=['GET', 'POST'] )
 def add_Question(category):
-    global Questions, last_id
-    if current_user.is_login:
-        if current_user.role == 'admin':
-            if request.method == 'POST':
-                Question = request.form.get('question')
-                Answer = request.form.get('answer')
-                Category = category
-                last_id += 1
-                Questions.append({'id': last_id, 'Q': Question, 'A': Answer, 'category': Category})
-                flash('Added Successfully', 'success')
-            return render_template ('add_q.html', category=category)
+    if admin_check('admin'):
+        if request.method == 'POST':
+            Question = request.form.get('question')
+            Answer = request.form.get('answer')
+            Category = category
+            cursor = get_db().cursor()
+            cursor.execute("INSERT INTO questions (category, question, answer) VALUES (?, ?, ?)",
+                (Category, Question, Answer))
+            get_db().commit()
+            flash('Added Successfully', 'success')
+        return render_template ('add_q.html', category=category)
     else:
         return render_template ('not_allowed.html')
 
 
 @app.route ('/questions/delet/<category>', methods = ['GET', 'POST'])
 def remove_Question(category):
-    global Questions
-    if current_user.is_login:
-        if current_user.role == 'admin':
-            if request.method == 'POST':
-                selected_ids = request.form.getlist('id')
-                Questions = [q for q in Questions if str(q['id']) not in selected_ids]
+    if admin_check('admin'):
+        cursor = get_db().cursor()
+        if request.method == 'POST':
+            selected_ids = request.form.getlist('id')
+            for id in selected_ids:
+                cursor.execute("DELETE FROM questions WHERE id=?",(id,))
+            get_db().commit()
 
-            filtered_questions = [q for q in Questions if q['category'] == category]
-            return render_template('delet_q.html', Questions=filtered_questions, category=category)
+        cursor.execute("SELECT * FROM questions WHERE category=?", (category,))
+        filtered_questions = cursor.fetchall()
+        return render_template('delet_q.html', Questions=filtered_questions, category=category)
     else:
         return render_template ('not_allowed.html')
 
 
 @app.route ('/categories/add', methods = ['GET', 'POST'])
 def add_category():
-    if current_user.is_login:
-        if current_user.role == 'admin':
-            if request.method == 'POST':
+    cursor = get_db().cursor()
+    if admin_check('admin'):
+        if request.method == 'POST':
 
-                new_category=request.form.get('category')
-                if next((True for q in Questions if q['category'] == new_category),False):
-                    flash('This category already exist', 'danger')
-                else:
-                    flash('Successfully created new category', 'success')
-                    return redirect(url_for('Question_manager', category=new_category))
-    
-            return render_template('add_category.html')
+            new_category=request.form.get('category')
+            cursor.execute("SELECT name FROM categories")
+            category_rows = cursor.fetchall()
+            categories = [category for (category,) in category_rows]
+            if next((True for category in categories if category == new_category),False):
+                flash('This category already exist', 'danger')
+            else:
+                cursor.execute("INSERT INTO categories (name) VALUES (?)", (new_category,))
+                get_db().commit()
+                flash('Successfully created new category', 'success')
+                return redirect(url_for('Question_manager', category=new_category))
+
+        return render_template('add_category.html')
     else:
         return render_template ('not_allowed.html')
 
 
 @app.route ('/categories/delet/<category>')
 def remove_category(category):
-    global Questions
-    if current_user.is_login:
-        if current_user.role == 'admin':
-            Questions = [q for q in Questions if q['category'] != category]
+    cursor = get_db().cursor()
+    if admin_check('admin'):
+            cursor.execute("DELETE FROM questions WHERE category=?", (category,))
+            cursor.execute("DELETE FROM categories WHERE name=?", (category,))
+            get_db().commit()
             flash('Category Removed', 'danger')
             return redirect(url_for('show_categories'))
     else:
